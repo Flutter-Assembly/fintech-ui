3eff4b864e292806f7d2b2320aae3d43552191d9
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2023-07-25 21:49:33 +0100

    init app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.fintech_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:fintech_ui/app/app.dart';
+import 'package:fintech_ui/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
